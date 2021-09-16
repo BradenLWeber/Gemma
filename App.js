b6080c1169c3bcfb4b77026036f8bf29b8dac7a8
rmd34 <rmd34@remotel2.cs.calvin.edu>
2021-09-16 12:26:58 -0400

    Icon idea and intro message

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image,  StyleSheet, Text, View } from 'react-native';
+import gemmaicon from './assets/gemmaidea1.jpg'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={gemmaicon} style={styles.logo} />
+
+      <Text style={styles.instructions} >
+        Hello, Team Furious Five!      
+      </Text>
     </View>
   );
 }
@@ -17,5 +20,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 200,
+    height: 250,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginBottom: 10,
   },
 });
