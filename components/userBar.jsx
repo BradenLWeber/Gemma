856ea0bc5dc82662b85c0d9f3d511deff5641b09
Braden Weber <bweber@celerik.com>
2021-09-24 09:00:08 -0400

    UserBar component

---
+++ components/userBar.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, KeyboardAvoidingView, TextInput } from 'react-native'
+
+const UserBar = (props) => {
+    return (
+        <View style={styles.userBar}>
+        <>
+            <View style={styles.userIcon}>
+                {/* user image */}
+            </View>
+            <KeyboardAvoidingView
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                style={styles.userInput}
+            >
+                <TextInput style={styles.input} placeholder={'Search for a pin...'}>
+
+                </TextInput>
+            </KeyboardAvoidingView>
+            <View style={styles.userOptions}>
+                <Text>- - -</Text>
+            </View>
+        </>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    userBar: {
+        width: '90%',
+        height: 60,
+        borderRadius: 30,
+        backgroundColor: '#BCBCBC',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    userIcon: {
+        width: 46,
+        height: 46,
+        borderRadius: 23,
+        backgroundColor: '#D4D8E5',
+        borderColor: '#393B48',
+        borderWidth: 2,
+        margin: 7,
+    },
+    userInput: {
+        flex: 1,
+        height: 40,
+    },
+    input: {
+        flex: 1,
+        borderRadius: 10,
+        backgroundColor: '#FFFFFF',
+        paddingLeft: 10,
+        paddingRight: 10,
+        borderColor: 'black',
+        borderWidth: 1,
+        fontSize: 20,
+    },
+    userOptions: {
+        width: 46,
+        height: 46,
+        borderRadius: 23,
+        margin: 7,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export default UserBar;
