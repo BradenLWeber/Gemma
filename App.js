f24805adc57fc37e3faf31f3379fb05ed1f00b46
Braden Weber <bweber@celerik.com>
2021-12-08 14:10:48 -0500

    Fix error message

--- App.js
+++ App.js
@@ -54,7 +54,6 @@ export default function App() {
     return (
       <UserLoginScreen
         navigator={navigation}
-        geoPermissions={route.params.geoPermissions}
       />
     )
   };
@@ -63,7 +62,6 @@ export default function App() {
     return (
       <UserSignupScreen
         navigator={navigation}
-        geoPermissions={route.params.geoPermissions}
       />
     )
   };
@@ -93,25 +91,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  addWrapper: {
-    width: 30,
-    height: 30,
-    backgroundColor: '#FFF',
-    borderRadius: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderWidth: 1
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-  },
-  PinButton: {
-    position: 'absolute',
-    bottom: 10,
-    right: 10,
-  },
-});
